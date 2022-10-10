d5243e702fcfe766dd9b1c19fc3130265d3eba8b
yaotailin <yaotailin@163.com>
2022-10-10 15:31:32 +0800

    基于Cglib实现含构造函数的类实例化策略

---
+++ small-spring-step-03/src/main/java/cn/mark/springframework/beans/factory/BeanFactory.java
@@ -0,0 +1,10 @@
+package cn.mark.springframework.beans.factory;
+
+import cn.mark.springframework.beans.BeansException;
+
+public interface BeanFactory {
+
+    Object getBean(String name) throws BeansException;
+
+    Object getBean(String name, Object... args) throws BeansException;
+}
---
+++ small-spring-step-03/src/test/java/cn/mark/springframework/test/ApiTest.java
@@ -0,0 +1,73 @@
+package cn.mark.springframework.test;
+
+import cn.mark.springframework.beans.factory.config.BeanDefinition;
+import cn.mark.springframework.beans.factory.support.DefaultListableBeanFactory;
+import cn.mark.springframework.test.bean.UserService;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.NoOp;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class ApiTest {
+
+    @Test
+    public void testBeanFactory() {
+        //1. 初始化beanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        //2. 注册bean
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.获取bean
+        UserService userService = (UserService) beanFactory.getBean("userService", "姚太林");
+        userService.queryUserInfo();
+    }
+
+    @Test
+    public void test_cglib() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(UserService.class);
+        enhancer.setCallback(new NoOp() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        });
+        Object obj = enhancer.create(new Class[]{String.class}, new Object[]{"姚太林"});
+        System.out.println(obj);
+    }
+
+    @Test
+    public void test_newInstance() throws IllegalAccessException, InstantiationException {
+        UserService userService = UserService.class.newInstance();
+        System.out.println(userService);
+    }
+
+    @Test
+    public void test_constructor() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class<UserService> userServiceClass = UserService.class;
+        Constructor<UserService> declaredConstructor = userServiceClass.getDeclaredConstructor(String.class);
+        UserService userService = declaredConstructor.newInstance("姚太林");
+        System.out.println(userService);
+    }
+
+    @Test
+    public void test_parameterTypes() throws Exception {
+        Class<UserService> beanClass = UserService.class;
+        Constructor<?>[] declaredConstructors = beanClass.getDeclaredConstructors();
+        Constructor<?> constructor = null;
+        for (Constructor<?> ctor : declaredConstructors) {
+            if (ctor.getParameterTypes().length == 1) {
+                constructor = ctor;
+                break;
+            }
+        }
+        Constructor<UserService> declaredConstructor = beanClass.getDeclaredConstructor(constructor.getParameterTypes());
+        UserService userService = declaredConstructor.newInstance("姚太林");
+        System.out.println(userService);
+    }
+
+}
